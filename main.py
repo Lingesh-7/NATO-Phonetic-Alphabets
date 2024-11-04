eb9e2ed4a21a7b5041333e76e3df873e6d53d4e9
Lingesh-7 <lingesh.r.official@gmail.com>
2024-11-04 12:30:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 
-#ODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-
-#ODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
 import pandas as p
 
-data=p.read_csv("nato_phonetic_alphabet.csv")
-
-# for i,r in data.iterrows():
-#     print(r)
-
+data=p.read_csv(r"C:\Users\linge\Desktop\python 100 days\projects\pr-26\nato_phonetic_alphabet.csv")
 credic={r.letter:r.code for i,r in data.iterrows()}
-# print(credic)
-
-instr=input("Enter a Word: ").upper()
-
-codeword=[credic[l] for l in instr]
-print(codeword)
 
 
-# cr=[]
-# for i in instr:
-#     for k,v in credic.items():
-#         if i==k:
-#             cr.append(v)
-# print(cr)
+#METHOD 1
+t=True
+while t:
+    instr=input("Enter a Word: ").upper()
+    try:
+        
+        codeword=[credic[l] for l in instr]
+    except KeyError:
+        print("Sorry only Letters in the alphabet please")
+        # t=True      
+    else:
+        print(codeword)
+        t=False
+        
+
+#METHOD 2
+def genalpha():
+    instr=input("Enter a Word: ").upper()
+    try:
+        
+        codeword=[credic[l] for l in instr]
+    except KeyError:
+        print("Sorry only Letters in the alphabet please")
+        genalpha()
+    else:
+        print(codeword)
+
+genalpha()
